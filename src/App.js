61d4132b4993882c44f88e19f520575f587dcfa8
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-05-03 00:51:41 +0200

    state init

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Car from './Car/Car'
 
 class App extends Component {
 
-    state = {
-        cars: [
-            {name: 'Ford', year: 2018},
-            {name: 'Audi', year: 2016},
-            {name: 'Mazda', year: 2010}
-        ],
-        pageTitle: 'React components',
-        showCars: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            cars: [
+                {name: 'Ford', year: 2018},
+                {name: 'Audi', year: 2016},
+                {name: 'Mazda', year: 2010}
+            ],
+            pageTitle: 'React components',
+            showCars: false
+        }
     }
 
     toggleCarsHandler = () => {
