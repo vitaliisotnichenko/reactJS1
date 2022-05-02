c01722542be6f63f830f0cb51c8746df15a0a42c
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-05-03 00:17:41 +0200

    commponent stylization

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react";
+import React, { Component } from 'react';
+import './App.module.scss';
 import Car from './Car/Car'
 
-function App() {
-  const divStyle = {
-      textAlign: 'center'
-  }
-  return (
-    <div style={divStyle}>
-      <h1>Hello</h1>
-      <Car name={'Ford'} year={2018}>
-          <p style={{color: 'blue'}}>Color</p>
-      </Car>
-      <Car name={'Audi'} year={2016}>
-          <p style={{color: 'red'}}>Color</p>
-      </Car>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2016},
+            {name: 'Mazda', year: 2010}
+        ],
+        pageTitle: 'React components',
+        showCars: false
+    }
+
+    toggleCarsHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    onChangeName(name, index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const cars = [...this.state.cars]
+        cars[index] = car
+        this.setState({cars})
+    }
+
+    deleteHandler(index) {
+        const cars = this.state.cars.concat()
+        cars.splice(index, 1)
+
+        this.setState({cars})
+
+    }
+
+    render() {
+        const divStyle = {
+            textAlign: 'center'
+        }
+
+        let cars = null
+
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onDelete={this.deleteHandler.bind(this, index)}
+                        onChangeName={event => this.onChangeName(event.target.value, index)}
+                    />
+                )
+            })
+        }
+
+        return (
+            <div style={divStyle}>
+                <h1>{this.state.pageTitle}</h1>
+
+                <button
+                    onClick={this.toggleCarsHandler}
+                >Toggle cars</button>
+
+                <div style={{
+                    width: 400,
+                    margin: 'auto',
+                    paddingTop: '20px'
+                }}>
+                    { cars }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
