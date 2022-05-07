ad25e3d0540e66a1636f736526ad7ce8743c237b
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-05-07 22:22:16 +0200

    Change state

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -7,17 +7,22 @@ export default class Counter extends Component {
     }
 
     addCounter = () => {
-        this.setState({
-            counter: this.state.counter + 1
+        // this.setState({
+        //     counter: this.state.counter + 1
+        // })
+        this.setState((prevState) => {
+            return {
+                counter: prevState.counter + 1
+            }
         })
     }
     render() {
         return (
-            <React.Fragment>
+            <Auxiliary>
                 <h2>Counter {this.state.counter}</h2>
                 <button onClick={this.addCounter}>+</button>
                 <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
-            </React.Fragment>
+            </Auxiliary>
         )
     }
 }
