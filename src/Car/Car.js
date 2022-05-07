d73bc8e362c06faa911cb3b447d6491219884762
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-05-07 23:53:47 +0200

    validation parameters with PropTypes

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './Car.css'
 import withClass from "../hoc/withClass";
+import PropTypes from 'prop-types'
 
 class Car extends React.Component {
     render() {
@@ -30,6 +31,13 @@ class Car extends React.Component {
             </React.Fragment>
         )
     }
+}
+
+Car.propTypes = {
+    name: PropTypes.string,
+    year: PropTypes.number,
+    onChangeName: PropTypes.func,
+    onDelete:PropTypes.func
 }
 
 export default withClass(Car, classes.Car)
