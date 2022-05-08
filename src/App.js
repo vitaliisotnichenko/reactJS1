2ea922297b4dc7425d61248982908d84105fabc8
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-05-08 18:51:41 +0200

    add references

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ class App extends Component {
                         <Car
                             name={car.name}
                             year={car.year}
+                            index={index}
                             onDelete={this.deleteHandler.bind(this, index)}
                             onChangeName={event => this.onChangeName(event.target.value, index)}
                         />
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,6 +4,18 @@ import withClass from "../hoc/withClass";
 import PropTypes from 'prop-types'
 
 class Car extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.inputRef = React.createRef()
+    }
+    componentDidMount() {
+        if(this.props.index === 1) {
+            this.inputRef.current.focus()
+        }
+    }
+
     render() {
         const inputClasses = ['input']
 
@@ -22,6 +34,7 @@ class Car extends React.Component {
                 <h3>Сar name: {this.props.name}</h3>
                 <p>Year: <strong>{this.props.year}</strong></p>
                 <input
+                    ref={this.inputRef}
                     type="text"
                     onChange={this.props.onChangeName}
                     value={this.props.name}
@@ -36,6 +49,7 @@ class Car extends React.Component {
 Car.propTypes = {
     name: PropTypes.string,
     year: PropTypes.number,
+    index: PropTypes.number,
     onChangeName: PropTypes.func,
     onDelete:PropTypes.func
 }
